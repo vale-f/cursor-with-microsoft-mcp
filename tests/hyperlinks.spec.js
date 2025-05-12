89c796233f90f25c7defe414830790259ae3191f
Valentina <valentinafava93@gmail.com>
2025-05-12 18:49:06 -0300

    Refactor tests: improve readability by using test.step()

--- tests/hyperlinks.spec.js
+++ tests/hyperlinks.spec.js
@@ -1,17 +1,22 @@
 import { test, expect } from '@playwright/test';
 
 test('Social media icons point to correct URLs', async ({ page }) => {
-  await page.goto('/');
+  await test.step('Go to the homepage', async () => {
+    await page.goto('/');
+  });
 
-  // Linkedin
-  const linkedinHref = await page.getAttribute('a[href="https://www.linkedin.com/company/open-knowledge-foundation/"]', 'href');
-  expect(linkedinHref).toBe('https://www.linkedin.com/company/open-knowledge-foundation/');
+  await test.step('Check LinkedIn icon points to correct URL', async () => {
+    const linkedinHref = await page.getAttribute('a[href="https://www.linkedin.com/company/open-knowledge-foundation/"]', 'href');
+    expect(linkedinHref).toBe('https://www.linkedin.com/company/open-knowledge-foundation/');
+  });
 
-  // YouTube
-  const youtubeHref = await page.getAttribute('a[href="https://www.youtube.com/@OpenKnowledgeFoundation/videos"]', 'href');
-  expect(youtubeHref).toBe('https://www.youtube.com/@OpenKnowledgeFoundation/videos');
+  await test.step('Check YouTube icon points to correct URL', async () => {
+    const youtubeHref = await page.getAttribute('a[href="https://www.youtube.com/@OpenKnowledgeFoundation/videos"]', 'href');
+    expect(youtubeHref).toBe('https://www.youtube.com/@OpenKnowledgeFoundation/videos');
+  });
 
-  // X
-  const xHref = await page.getAttribute('a[href="https://twitter.com/okfn"]', 'href');
-  expect(xHref).toBe('https://twitter.com/okfn');
+  await test.step('Check X icon points to correct URL', async () => {
+    const xHref = await page.getAttribute('a[href="https://twitter.com/okfn"]', 'href');
+    expect(xHref).toBe('https://twitter.com/okfn');
+  });
 });
