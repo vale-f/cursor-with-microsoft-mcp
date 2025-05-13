8c6896e18ff4310cd764b164f6613efdb911b82b
Valentina <valentinafava93@gmail.com>
2025-05-12 21:32:38 -0300

    Add Page Object Model structure

--- tests/language-switch.spec.js
+++ tests/language-switch.spec.js
@@ -1,53 +1,42 @@
 const { test, expect } = require('@playwright/test');
+const { HomePage } = require('./page-objects/HomePage');
 
 test('Language switch from Spanish to English', async ({ page }) => {
+  const homePage = new HomePage(page);
+
   await test.step('Go to the Spanish homepage', async () => {
-    await page.goto('/es/');
+    await homePage.goto('es');
   });
 
   await test.step('Verify the Spanish heading is present', async () => {
-    await expect(
-      page.getByRole('heading', { name: /Estamos construyendo un mundo abierto por diseño/ })
-    ).toBeVisible();
+    await homePage.expectSpanishHeading();
   });
 
-  await test.step('Hover over the language icon to reveal the English link', async () => {
-    await page.hover('img[alt="Language"].max-w-none');
-  });
-
-  await test.step('Click the English link', async () => {
-    await page.click('a[href="/en/"]');
+  await test.step('Switch to English', async () => {
+    await homePage.switchToEnglish();
   });
 
   await test.step('Verify the English heading is present', async () => {
-    await expect(
-      page.getByRole('heading', { name: /We are building a world open by design where all knowledge is/ })
-    ).toBeVisible();
+    await homePage.expectEnglishHeading();
   });
 });
 
 test('Language switch from English to Spanish', async ({ page }) => {
+  const homePage = new HomePage(page);
+
   await test.step('Go to the English homepage', async () => {
-    await page.goto('/en/');
+    await homePage.goto('en');
   });
 
   await test.step('Verify the English heading is present', async () => {
-    await expect(
-      page.getByRole('heading', { name: /We are building a world open by design where all knowledge is/ })
-    ).toBeVisible();
+    await homePage.expectEnglishHeading();
   });
 
-  await test.step('Hover over the language icon to reveal the Spanish link', async () => {
-    await page.hover('img[alt="Language"].max-w-none');
-  });
-
-  await test.step('Click the Spanish link', async () => {
-    await page.click('a[href="/es/"]');
+  await test.step('Switch to Spanish', async () => {
+    await homePage.switchToSpanish();
   });
 
   await test.step('Verify the Spanish heading is present', async () => {
-    await expect(
-      page.getByRole('heading', { name: /Estamos construyendo un mundo abierto por diseño/ })
-    ).toBeVisible();
+    await homePage.expectSpanishHeading();
   });
 });
