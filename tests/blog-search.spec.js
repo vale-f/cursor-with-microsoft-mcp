be3123d9724729161ff67ea75d5de27e7e651014
Valentina <valentinafava93@gmail.com>
2025-05-14 13:31:26 -0300

    Complete test for the blog search bar feature

--- tests/blog-search.spec.js
+++ tests/blog-search.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const { HomePage } = require('./page-objects/HomePage');
 
-test('User can search for terms using the blog search bar', async ({ page }) => {
+test('Blog: user can search for terms using the blog search bar', async ({ page }) => {
     const homePage = new HomePage(page);
     await test.step('Go to the homepage', async () => {
       await page.goto('en');
@@ -16,7 +16,13 @@ test('User can search for terms using the blog search bar', async ({ page }) => 
         await searchInput.fill('open data');
         await page.locator('li#search-3 button[type="submit"].search-bar_button').click({ waitFor: 'load' });
       });
-      //await test.step('Verify that the term is present in the first five results', async () => {
-        //
-      //});
+      await test.step('Verify that the term is present in the titles of the results on the first page', async () => {
+        const titles = await page.locator('.entry-title');
+        const count = await titles.count();
+
+        for (let i = 0; i < count; i++) {
+          const titleText = await titles.nth(i).innerText();
+          expect(titleText.toLowerCase()).toContain('open data');
+        }
+      });
   });
